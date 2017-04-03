87071f714ca62fe4ceeb9895040baee3ee3b07e5
dayeonhwang <dahwang2018@u.northwestern.edu>
2017-04-03 18:53:31 -0500

    add access code authentication

---
+++ main.js
@@ -0,0 +1,10 @@
+function scheduleCheck() {
+    var accessCode = prompt("Please enter your access code", "whyhellothere :D");
+    var scheduleText = document.getElementById("schedule-text");
+    if (accessCode !== null && accessCode === "buildherup") {
+        	scheduleText.style.display = 'inline';
+        	document.getElementById("schedule-btn-box").style.display ='none';
+        } else  {
+    	alert("Invalid access code");
+    }
+}
