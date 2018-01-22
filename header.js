f81359181f8b41731571f73a4ba9b3e007aa4945
Nancy Yao <nancy.yao2@gmail.com>
2018-01-21 18:53:45 -0600

    2018 Website update

---
+++ header.js
@@ -0,0 +1,13 @@
+document.write('\
+\
+    <title>BuildHer Hackathon</title>\
+	<link rel="stylesheet" href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>\
+    <link rel='stylesheet' href ='style.css' type='text/css'>\
+    <!--Fonts-->\
+    <link href='https://fonts.googleapis.com/css?family=Bungee+Inline|Lobster|Muli|Raleway|Encode+Sans+Semi+Expanded' rel="stylesheet">\
+	<!-- jQuery library -->\
+	<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>\
+	<!-- Latest compiled JavaScript -->\
+	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>\
+\
+');
